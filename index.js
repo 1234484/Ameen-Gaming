caf3dc5cde8dfbf4464b1bfef17f133e38e70581
Ameen Nadeem <ameennadeem134@gmail.com>
2024-07-03 13:48:04 +0500

    Ameen Gamings

---
+++ index.js
@@ -0,0 +1,6 @@
+const container=docoment.querySelector('.container');
+const LoginLink=docoment.querySelector('.SignInLink');
+const RegisterLink=docoment.querySelector('.SignUpLink');
+		RegisterLink.addEventListener('click' , ()=>{
+				container.classList.add('active');
+		})
